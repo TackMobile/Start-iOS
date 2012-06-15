d44b8f56662f5819a7589ea24892f64b92317778
Nick Place <nick@manyplaces.net>
2012-06-15 08:58:27 -0600

    Initial Commit

---
+++ Start/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Start
+//
+//  Created by Nick Place on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
